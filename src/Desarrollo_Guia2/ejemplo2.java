964729c5f82e3113bf434093403a7466fea13537
GerardoM3 <gerardomonroy13@gmail.com>
2022-03-15 16:04:05 -0600

    Comenzar Ejemplo 5 de la Guía Práctica 2

--- src/Desarrollo_Guia2/ejemplo2.java
+++ src/Desarrollo_Guia2/ejemplo2.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Desarrollo_Guia2;
 
-/**
- *
- * @author ITCA
- */
 public class ejemplo2 {
+    public static void main(String[] args) {
+        System.out.println("Método void");
+        metodoConParametrosVoid(1, 24.34, "Lucas");
+        System.out.println("---------------------------");
+        System.out.println("Método int");
+        metodoConParametrosInt(1);
+        System.out.println("---------------------------");
+        System.out.println("Método double");
+        metodoConParametrosDouble(24.34);
+        System.out.println("---------------------------");
+        System.out.println("Método string");
+        metodoConParametrosString("Lucas");
+    }
     
+    public static void metodoConParametrosVoid(int valorInt, double valorDouble, String valorString){
+        System.out.println("Valor int: " + valorInt);
+        System.out.println("Valor double: " + valorDouble);
+        System.out.println("Valor string: " + valorString);
+    }
+    
+    public static int metodoConParametrosInt(int valorInt){
+        System.out.println("El valor int es: " + valorInt);
+        return valorInt;
+    }
+    
+    public static double metodoConParametrosDouble(double valorDouble){
+        System.out.println("El valor double es: " + valorDouble);
+        return valorDouble;
+    }
+    
+    public static String metodoConParametrosString(String valorString){
+        System.out.println("El valor string es: " + valorString);
+        return valorString;
+    }
 }
---
+++ src/Desarrollo_Guia2/ejemplo5.java
@@ -0,0 +1,15 @@
+
+package Desarrollo_Guia2;
+
+import java.util.Scanner;
+
+
+public class ejemplo5 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        double valorMaquina, valorFlete, valorSeguro;
+        System.out.println("Valor de la máquina: ");
+        valorMaquina = leer.nextDouble();
+        System.out.println("");
+    }
+}
