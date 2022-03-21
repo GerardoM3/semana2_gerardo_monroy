b675479703c5940a3b284ea7b087654f3c3adfac
GerardoM3 <gerardomonroy13@gmail.com>
2022-03-20 23:00:26 -0600

    A comenzar el ejercicio 5 de la Guía Práctica 2

---
+++ src/Ejercicios_Guia2/ejercicio5.java
@@ -0,0 +1,16 @@
+
+package Ejercicios_Guia2;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        NumberFormat formato = new DecimalFormat("#.##");
+        
+        
+    }
+}
